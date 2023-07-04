c19c6bd21ff9a060268dad19d6d9a253989a0a71
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-04 14:03:56 +0530

    added initial router

--- codeial-kus/index.js
+++ codeial-kus/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
-
 const PORT = 8000;
-
 const app = express();
+
+//Setting up routes in different folder
+app.use('/', require('./routes')); //This will invoke index.js inside ./route by default.
 
 app.listen(PORT, (err)=>{
     if(err){
