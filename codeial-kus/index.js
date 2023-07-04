72192bcf7d5d2cb658c857f83a5404987e8f1ec8
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-04 23:43:18 +0530

    added ejs and setup ejs

--- codeial-kus/index.js
+++ codeial-kus/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
+const path = require("path");
+
 const PORT = 8000;
 const app = express();
+
+//setting us the view engine
+app.set("view engine",  "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 //Setting up routes in different folder
 app.use('/', require('./routes')); //This will invoke index.js inside ./route by default.
