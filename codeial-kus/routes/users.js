f35c0692ea5c30ac018d0ccf4c94c5f3b257f1f1
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-07 12:36:35 +0530

    Manual Auth: Added route for register and login function + Refactoring

--- codeial-kus/routes/users.js
+++ codeial-kus/routes/users.js
@@ -6,6 +6,9 @@ const usersController = require('../controllers/user_controller');
 router.get('/profile', usersController.profile);
 
 router.get('/sign-up', usersController.getSignUp);
-router.get('/sign-in', usersController.getSignIn)
+router.get('/sign-in', usersController.getSignIn);
+
+router.post('/login', usersController.login);
+router.post('/register', usersController.register);
 
 module.exports = router;
