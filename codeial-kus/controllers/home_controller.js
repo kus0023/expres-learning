7b360cc986975898c81e06d407b65ae02ae2db95
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-09 02:20:26 +0530

    Design comleted for comment section: Live data pending

--- codeial-kus/controllers/home_controller.js
+++ codeial-kus/controllers/home_controller.js
@@ -7,7 +7,7 @@ module.exports.home = async function (req, res) {
     try {
         const postDocs = await Post.find({}).populate('user');
 
-        return res.render("home", {title: "Home page", posts: postDocs});
+        return res.render("home", {title: "Home page", posts: postDocs, user: req.user});
     } catch (err) {
         console.log("Could not fetch Posts", err);
         return res.status(500).send("Internal Server Error.");
