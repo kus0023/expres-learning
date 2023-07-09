1503365285aa65e927f7592513860fb5848746eb
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-09 23:34:04 +0530

    profile update and design added.

--- codeial-kus/controllers/home_controller.js
+++ codeial-kus/controllers/home_controller.js
@@ -1,6 +1,7 @@
 //module.exports.actions = fn
 
 const Post = require('../models/Post');
+const User = require('../models/User');
 
 module.exports.home = async function (req, res) {
 
@@ -8,7 +9,9 @@ module.exports.home = async function (req, res) {
         const postDocs = await Post.find({}).populate('user')
         .populate({path: 'comments', model: 'Comment', populate: 'user'}).sort({createdAt: -1});
 
-        return res.render("home", {title: "Home page", posts: postDocs, user: req.user});
+        const friendsDoc = await User.find({});
+
+        return res.render("home", {title: "Home page", posts: postDocs, user: req.user, friends: friendsDoc});
     } catch (err) {
         console.log("Could not fetch Posts", err);
         return res.status(500).send("Internal Server Error.");
--- codeial-kus/routes/users.js
+++ codeial-kus/routes/users.js
@@ -4,7 +4,8 @@ const passport = require('passport');
 
 const usersController = require('../controllers/user_controller');
 
-router.get('/profile', passport.checkAuthentication, usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.viewProfile);
+router.post('/profile', passport.checkAuthentication, usersController.updateProfile)
 
 router.get('/sign-up', passport.checkAlreadyLoggedIn, usersController.getSignUp);
 router.get('/sign-in', passport.checkAlreadyLoggedIn, usersController.getSignIn);
