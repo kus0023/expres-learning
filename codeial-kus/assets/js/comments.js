955caee821a20be247cde1afa792cc7614974fe4
Kumar Utsav Singh <utsav.singh0023@gmail.com>
2023-07-13 00:08:33 +0530

    AJAX: comment addition with ajax

--- codeial-kus/assets/js/comments.js
+++ codeial-kus/assets/js/comments.js
@@ -1,6 +1,7 @@
 
 
 {
+    console.log($("#comment-forms form"));
     //This will get comment form id of particular post on which we will post the request.
     $("#comment-forms form").submit(function (e) {
         e.preventDefault();
@@ -26,15 +27,36 @@
 
     function handleSuccessResponse(data){
         //Add the new form in comment list DOM after successful response
-        console.log(data);
+        // console.log(data);
 
         const post = data.result.updatedPostDoc;
         const comment = data.result.commentDoc;
 
-        $('#comment_grp_'+post._id).append(createElement(post, comment)); //This will append the new child in comment.
+        let elementComment = $('#comment_grp_'+post._id).append(createElement(post, comment)); //This will append the new child in comment.
         $('#post_comment_btn_badge_'+post._id).html(post.comments.length); //This will update the button badge.
         $('#comment_'+post._id).removeClass("show").addClass("show"); // This will show the comment box.
         $('html, body').scrollTop($('#comment_'+post._id).offset().top); //Scroll to the comment.
+
+
+
+        deleteCommentLinkButton($(' .comment-delete-link', elementComment));
+    }
+
+    function deleteCommentLinkButton(link){
+        $(link).click(function(e){
+            e.preventDefault();
+
+            $.ajax({
+                type: 'get',
+                url: $(link).prop('href'),
+                success: function(data){
+                    console.log(data);
+                },
+                error: function(err){
+                    console.log(err);
+                }
+            })
+        })
     }
 
     
@@ -48,7 +70,7 @@
                                 <div class="row justify-content-between">
                                     <p class="mb-1 col-8">${ comment.content }</p>
                                     <div class="text-end col-2">
-                                        <a href="/comments/delete?post_id=${ post.id }&comment_id=${ comment.id }" class="btn btn-sm btn-danger" >Delete</a>  
+                                        <a href="/comments/delete?post_id=${ post.id }&comment_id=${ comment.id }" class="btn btn-sm btn-danger comment-delete-link" >Delete</a>  
                                     </div>
                                 </div>
                             </div>`
